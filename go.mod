19113c45463020f6faf73c708ed8ceba4e506dce
davi.pontes <davi.pontes@lsd.ufcg.edu.br>
2024-04-09 08:06:29 -0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Daviiap/lib
+
+go 1.21.1
---
+++ init.go
@@ -0,0 +1,14 @@
+/**
+ * Author: Davi Pontes
+ * File: init.go
+ */
+
+package lib
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("[go-lib] initializing ...")
+}
